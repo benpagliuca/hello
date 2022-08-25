4b696e5f4ba4c34b0e8d07bd535145ebf1346747
benpagliuca <111937328+benpagliuca@users.noreply.github.com>
2022-08-25 08:54:01 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Ben Pagliuca
+ * Date: 08/25/2022
+ **/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello, Im Ben Pagliuca!\n");
+    printf("My major is computer science!\n");
+
+    return 0;
+ }
